c9e4d14e6ebf868914b0d21c4b41a23793304eed
EC2 Default User <ec2-user@ip-172-31-15-72.ap-northeast-1.compute.internal>
2022-11-22 12:48:38 +0000

    genres作成

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,2 +1,17 @@
 class Admin::GenresController < ApplicationController
+  
+  
+  def index
+    @genre = Genre.new
+    @genres = Genre.all
+  end
+  
+  def edit
+  end
+  
+  def create
+  end
+  
+  def show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,15 @@ Rails.application.routes.draw do
   sessions: 'public/sessions'
 }
 
+
 # 管理者用
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
 }
+
+namespace :admin do
+resources :genres,only: [:index, :edit, :create, :update, :show]
+end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
