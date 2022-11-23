bf4bce82b0af7c12e4d18616cce69c07f6cb7b64
EC2 Default User <ec2-user@ip-172-31-15-72.ap-northeast-1.compute.internal>
2022-11-23 12:37:37 +0000

    ビュー更新

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,17 +1,18 @@
 class Admin::GenresController < ApplicationController
-  
-  
+
+
   def index
     @genre = Genre.new
     @genres = Genre.all
   end
-  
+
   def edit
+    @genre = Genre.find(params[:id])
   end
-  
+
   def create
   end
-  
+
   def show
   end
 end
