2a7fcc3ca509b62b9697447f2ec8953622875183
EC2 Default User <ec2-user@ip-172-31-15-72.ap-northeast-1.compute.internal>
2022-11-24 07:16:49 +0000

    ジャンル登録機能修正

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -11,8 +11,31 @@ class Admin::GenresController < ApplicationController
   end
 
   def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      redirect_to admin_genres_path
+    else
+      @genres = Genre.all
+      render :'index'
+    end
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      redirect_to admin_genres_path
+    else
+      render :"edit"
+    end
   end
 
   def show
   end
+
+private
+
+def genre_params
+    params.require(:genre).permit(:name)
+end
+
 end
