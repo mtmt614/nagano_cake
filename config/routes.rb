e9dffaddb4915387177b1c16d89f250b0634460b
EC2 Default User <ec2-user@ip-172-31-15-72.ap-northeast-1.compute.internal>
2022-12-04 11:41:49 +0000

    customerのviews更新

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
 namespace :admin do
 resources :genres, only: [:index, :edit, :create, :update, :show]
 resources :items, only: [:index, :create, :new, :show, :edit, :update]
+resources :customers, only: [:index, :show, :edit, :update]
 end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
