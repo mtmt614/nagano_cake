d9a66ae77ee0ca1ea584f2c25c05ca6a3366e8ac
EC2 Default User <ec2-user@ip-172-31-15-72.ap-northeast-1.compute.internal>
2022-12-11 12:27:29 +0000

    レイアウト変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :genre
-  has_one_attached :item_image
+  
+  has_one_attached :image
+  
+  def sale_status_method
+    if sale_status == true
+      '販売中'
+    else
+      '販売停止中'
+    end
+  end
   
   def with_tax_price
     (price * 1.1).floor
