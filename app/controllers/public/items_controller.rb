77066c6061f533add222d5962e71cab3a6c436ee
EC2 Default User <ec2-user@ip-172-31-15-72.ap-northeast-1.compute.internal>
2022-12-12 07:07:49 +0000

    itemsレイアウト更新

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -3,7 +3,7 @@ class Public::ItemsController < ApplicationController
   def index
     @genres = Genre.all
     @all_items = Item.all
-    @items = Item.page(params[:page]).per(8)
+    @items = Item.page(params[:page]).per(10)
   end
 
   def show
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,26 @@
 class Item < ApplicationRecord
-  belongs_to :genre
   
+  belongs_to :genre
+
   has_one_attached :image
   
-  def sale_status_method
-    if sale_status == true
+  def get_image
+    if image.attached?
+      image
+    else
+      'no_image.jpg'
+    end
+  end
+
+
+  def is_active_method
+    if is_active == true
       '販売中'
     else
       '販売停止中'
     end
   end
-  
+
   def with_tax_price
     (price * 1.1).floor
   end
