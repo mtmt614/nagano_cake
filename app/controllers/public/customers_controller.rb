1021a4544ae28f1fcba02a22842f842746702388
EC2 Default User <ec2-user@ip-172-31-15-72.ap-northeast-1.compute.internal>
2022-12-13 11:14:10 +0000

    コントローラ作成

---
+++ app/controllers/public/customers_controller.rb
@@ -0,0 +1,10 @@
+class Public::CustomersController < ApplicationController
+  def show
+    @customer = Customer.find(current_customer.id)
+  end
+
+  def edit
+    @customer = Customer.find(current_customer.id)
+  end
+
+end
