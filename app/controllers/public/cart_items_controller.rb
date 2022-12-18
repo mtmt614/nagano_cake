fa83c2c0b8d1187837c00c443925b02d564b672d
EC2 Default User <ec2-user@ip-172-31-15-72.ap-northeast-1.compute.internal>
2022-12-18 07:05:15 +0000

    カート機能修正

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -6,6 +6,20 @@ class Public::CartItemsController < ApplicationController
 
   def update
     @cart_item = CartItem.find(params[:id])
+    if @cart_item.update(cart_item_params)
+      redirect_to cart_items_path 
+    else
+      redirect_to request.referer
+    end
+    if @cart_item.amount
+       @cart_item.destroy
+    end
+  end
+  
+  def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to cart_items_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -20,14 +20,19 @@ end
 
 get 'customers/my_page' => 'public/customers#show'
 
-  devise_for :customers, skip: [:passwords], controllers: {
+  devise_for :customers, skip: [:encrypted_passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
 
 scope module: :public do
-resources :cart_items, only: [:index, :destroy, :update, :create]
+  
+resources :cart_items, only: [:index, :destroy, :update, :create]do
+  collection do
 delete "/cart_items/destroy_all" => "cart_items#destroy_all"
+end
+end
+
 resources :items, only: [:index, :show]
 end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
