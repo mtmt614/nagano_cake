924d5aacfc1d1edbeecd3f59c6161674e9e81e7c
EC2 Default User <ec2-user@ip-172-31-15-72.ap-northeast-1.compute.internal>
2022-12-20 06:29:06 +0000

    create部分更新

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -2,24 +2,45 @@ class Public::CartItemsController < ApplicationController
 
   def index
     @cart_items = current_customer.cart_items
+    @total = 0
   end
 
   def update
     @cart_item = CartItem.find(params[:id])
     if @cart_item.update(cart_item_params)
-      redirect_to cart_items_path 
+      redirect_to cart_items_path
     else
       redirect_to request.referer
     end
-    if @cart_item.amount
+    if @cart_item.amount == 0
        @cart_item.destroy
     end
   end
-  
+
   def destroy
     @cart_item = CartItem.find(params[:id])
     @cart_item.destroy
     redirect_to cart_items_path
+  end
+
+  def create
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+    @cart_items = current_customer.cart_items.all
+    @cart_items.each do |cart_item|
+      if @cart_item.item_id == cart_item.item_id
+        if @cart_item.amount.present?
+          cart_item.update(amount: @cart_item.amount + cart_item.amount)
+          redirect_to cart_items_path
+        end
+      end
+    end
+    
+    if @cart_item.amount.present?
+      @cart_item.save
+      redirect_to cart_items_path
+    else
+      redirect_to item_path(@cart_item.item)
+    end
   end
 
   private
