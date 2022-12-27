58406d7cee75c28d32a1c0064ccbaf227f2e98c1
EC2 Default User <ec2-user@ip-172-31-15-72.ap-northeast-1.compute.internal>
2022-12-27 12:14:13 +0000

    [Start]注文ページ作成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,15 @@
 class Public::OrdersController < ApplicationController
+  
+  def new
+    @order = Order.new
+    unless current_customer.cart_items.present?
+      redirect_to root_path
+    end
+  end
+  
+  private
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :name, :address, :customer_id, :status, :shipping_cost)
+  end
+  
 end
