b482d06bf013fa093a5ec781dadb9858780af3df
EC2 Default User <ec2-user@ip-172-31-15-72.ap-northeast-1.compute.internal>
2022-12-30 14:51:24 +0000

    注文入力画面更新

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,6 +4,19 @@ class Public::OrdersController < ApplicationController
     @order = Order.new
   end
   
+  def index
+    @orders = current_customer.orders.all
+  end
+  
+  def show
+    @params = params[:check]
+    if @params
+      @order = Order.find(params[:id])
+    else
+      redirect_to root_path
+    end
+  end
+  
   private
   def order_params
     params.require(:order).permit(:payment_method, :postal_code, :name, :address, :customer_id, :status, :shipping_cost)
