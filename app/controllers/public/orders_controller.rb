619244bee8286022372a5c6a1596d31da1d00a74
EC2 Default User <ec2-user@ip-172-31-15-72.ap-northeast-1.compute.internal>
2023-01-07 14:49:25 +0000

    支払方法更新

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,6 +4,14 @@ class Public::OrdersController < ApplicationController
     @order = Order.new
   end
   
+  def confirm
+    @order = Order.new(order_params)
+    @address = Address.find(params[:order][:address_id])
+    @order.postal_code = current_customer.postal_code
+    @order.address = current_customer.address
+    @order.name = current_customer.first_name + current_customer.last_name
+  end
+  
   def index
     @orders = current_customer.orders.all
   end
