7ab21b5422319a0fef9f0f6a81838687eb12385f
EC2 Default User <ec2-user@ip-172-31-15-72.ap-northeast-1.compute.internal>
2023-01-15 12:49:55 +0000

    レイアウト変更,ボタン設置

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,6 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details, dependent: :destroy
   
-  enum payment_method: { credit_card: 0, transfer: 1 }
+  enum payment_method: { クレジットカード: 0, 銀行振込: 1 }
 
 end
