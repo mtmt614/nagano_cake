855f623bdced53e5b3f16ae6eac4def98493838f
EC2 Default User <ec2-user@ip-172-31-15-72.ap-northeast-1.compute.internal>
2023-01-16 12:48:31 +0000

    レイアウト更新

--- app/controllers/admin/oder_details_controller.rb
+++ app/controllers/admin/oder_details_controller.rb
@@ -1,2 +1,11 @@
 class Admin::OderDetailsController < ApplicationController
+  
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:status)
+  end
 end
