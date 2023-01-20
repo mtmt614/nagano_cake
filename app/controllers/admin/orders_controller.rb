90b3eabaa3f93466437af7c9927d274c618f09b6
EC2 Default User <ec2-user@ip-172-31-15-72.ap-northeast-1.compute.internal>
2023-01-20 11:55:14 +0000

    購入者名～注文ステータスまでの追加

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,6 +2,22 @@ class Admin::OrdersController < ApplicationController
   
   def show
     @order = Order.find(params[:id])
+    @orders = Order.all
+    @order_details = OrderDetail.where(order_id: @order)
+    @making_status = @order.order_details.pluck(:making_status)
+  end
+  
+  def update
+    @order = Order.find(params[:id])
+    @order_details = OrderDetail.where(order_id: @order)
+    if @order.update(order_status_params)
+      if @order.status.include?("入金確認")
+         @order_details.update( making_status: 1)
+      end
+    redirect_to admin_order_path(@order)
+    else
+      render "show"
+    end
   end
   
   private
