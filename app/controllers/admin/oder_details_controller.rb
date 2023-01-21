fb9f00e520f69894ab901f5b6c0318393873efe4
EC2 Default User <ec2-user@ip-172-31-15-72.ap-northeast-1.compute.internal>
2023-01-21 11:17:47 +0000

    レイアウト調整

--- app/controllers/admin/oder_details_controller.rb
+++ app/controllers/admin/oder_details_controller.rb
@@ -3,9 +3,28 @@ class Admin::OderDetailsController < ApplicationController
   def show
     @order = Order.find(params[:id])
   end
+  
+  def update
+    @order_detail = OrderDetail.find(params[:id])
+    
+    @order_detail.update(order_detail_params)
+    @order = @order_detail.order
+    
+    if @order_detail.making_status == "製作中"
+      @order.update(status: 2)
+      @order.save
+    end
+    
+    if @order.order_details.count == @order.order_details.where(making_status: 3).count
+      @order.update(status: 3)
+      @order.save
+    end
+    
+    redirect_back(fallback_location: root_path)
+  end
 
   private
-  def order_params
-    params.require(:order).permit(:status)
+  def order_detail_params
+    params.require(:order_detail).permit(:order_id, :making_status, :count)
   end
 end
