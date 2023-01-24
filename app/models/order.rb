4a827a831e4c834c732d2e727e42cb0555de3bd4
EC2 Default User <ec2-user@ip-172-31-15-72.ap-northeast-1.compute.internal>
2023-01-24 07:47:31 +0000

    記述ミス修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,17 @@
 class Order < ApplicationRecord
-  
+
   belongs_to :customer
   has_many :order_details, dependent: :destroy
-  
+
   enum payment_method: { credit_card: 0, transfer: 1 }
-  
+
   enum status: {"入金待ち": 0,"入金確認": 1,"製作中": 2,"発送準備中": 3, "発送済み": 4}
 
+  def order_amount
+    @total = 0
+    self.order_details.each do |order_detail|
+      @total += order_detail.amount
+    end
+    return @total
+  end
 end
